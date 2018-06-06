0da7df4a5f6b0e5e2a35c855ebef72b99d0843fa
retaze <lars.gruber@outlook.com>
2018-06-06 19:26:18 +0200

    changing alter from int to String

--- Hello/src/hello/Test.java
+++ Hello/src/hello/Test.java
@@ -6,14 +6,14 @@ public class Test {
 	public static void main(String[] args) {
 
 		String name;
-		int alter;
+		String alter;
 		String ort;
 		Scanner s = new Scanner(System.in);
 				
 		System.out.println("Bitte geben Deinen Namen ein:");
 		name = s.nextLine();
 		System.out.println("Wie alt bist du?");
-		alter = s.nextInt();
+		alter = s.nextLine();
 		System.out.println("Wo wohnst du?");
 		ort = s.nextLine();
 		System.out.println("Hallo "+name+". Du bist "+alter+" Jahre alt und wohnst in "+ort);		
