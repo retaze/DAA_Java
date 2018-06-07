f37102053890490488fc699129483d23449d97b5
Lars Gruber <Lars Gruber@AD-FIAE10>
2018-06-07 08:41:00 +0200

    String alter in in alter

--- Hello/src/hello/Test.java
+++ Hello/src/hello/Test.java
@@ -6,14 +6,14 @@ public class Test {
 	public static void main(String[] args) {
 
 		String name;
-		String alter;
+		int alter;
 		String ort;
 		Scanner s = new Scanner(System.in);
 				
 		System.out.println("Bitte geben Deinen Namen ein:");
 		name = s.nextLine();
 		System.out.println("Wie alt bist du?");
-		alter = s.nextLine();
+		alter = s.nextInt();
 		System.out.println("Wo wohnst du?");
 		ort = s.nextLine();
 		System.out.println("Hallo "+name+". Du bist "+alter+" Jahre alt und wohnst in "+ort);		
