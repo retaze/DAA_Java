d3634f96eeccd6fb4022786cb827caffef292e21
Lars Gruber <lars.gruber@outlook.com>
2018-08-14 09:25:38 +0200

    DAA changes

--- Hello/src/hello/Functions.java
+++ Hello/src/hello/Functions.java
@@ -12,19 +12,9 @@ public class Functions {
         z1 = sc.nextInt();
         z2 = sc.nextInt();
 
-        max = maximum(z1, z2);
+        max = Funktionen.maximum(z1, z2);
 
         System.out.print(max + " ist das Maximum");
 
         }
-
-    public static int maximum( int z1, int z2){
-        int max = 0;
-        if (z1 > z2) {
-            max = z1;
-        } else {
-            max = z2;
-        }
-        return max;
-    }
 }
---
+++ Hello/src/hello/Funktionen.java
@@ -0,0 +1,17 @@
+package hello;
+
+import java.util.Scanner;
+
+public class Funktionen {
+
+
+    public static int maximum( int z1, int z2){
+        int max = 0;
+        if (z1 > z2) {
+            max = z1;
+        } else {
+            max = z2;
+        }
+        return max;
+    }
+}
