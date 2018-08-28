95dcf93b16687995d47b45a75a094f8c04ea7b80
Lars Gruber <lars.gruber@outlook.com>
2018-08-28 14:51:34 +0200

    DAA changes

--- Hello/src/hello/Functions.java
+++ Hello/src/hello/Functions.java
@@ -22,13 +22,38 @@ public class Functions {
 
         max = mathlib.minimum(z1, z2);
 
-        System.out.print(max + " ist das Minimum");*/
+        System.out.print(max + " ist das Minimum");
 
         int a = 105;
         int b = 49;
 
         System.out.println("Test der mathematischen Funktionen\n");
-        System.out.println("Kleinstes gemeinsames Vielfaches von: " + a + " und " + b + " ist: " + mathlib.kgV(a, b));
+        System.out.println("Kleinstes gemeinsames Vielfaches von: " + a + " und " + b + " ist: " + mathlib.kgV(a, b));*/
+        float anschaffungswert = 0.0f;
+        float nutzungsdauer = 0.0f;
+        float restwert = 0.0f;
+        float abschreibung = 0.0f;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Lineare Abschreibung\n");
+        System.out.print("Bitte geben sie den Anschaffungswert ein:\n");
+
+        anschaffungswert = sc.nextFloat();
+
+        System.out.println("Bitte geben sie die Nutzungsdauer ein:\n");
+
+        nutzungsdauer = sc.nextFloat();
+
+        abschreibung = anschaffungswert / nutzungsdauer;
+        System.out.println("Nutzungsjahr \t Restwert");
+        while (anschaffungswert > 0)
+        {
+            anschaffungswert = anschaffungswert - abschreibung;
+            nutzungsdauer--;
+            System.out.println(nutzungsdauer + "\t\t\t\t " + anschaffungswert);
+        }
+
 
 
     }
