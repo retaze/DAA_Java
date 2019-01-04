cf32989a7a8a0176026586f606f63168e0a90949
daafisi <daafisidaa@gmail.com>
2019-01-04 15:39:37 +0100

    Werte fuer Kartenprint werden jetzt aus dem Array ausgelesen

--- Hello/src/Game/Karte.java
+++ Hello/src/Game/Karte.java
@@ -1,35 +1,40 @@
 package Game;
 
 public class Karte {
-    public static void main(String args[]) {
-
-
-        System.out.println("Test");
+    public static void main(String[] args) {
         //Hier teste ich wie die Ausgabe einer Kartenfunktion aussehen koennte
         //Ich moechte die die Felder mit Zeichen darstellen
         //Loop in loop geben die felder Zeile fuer Zeile aus
 
+        //zweidimensionaler Array aus chars der das Feld representiert
         char[][] kartenArray = new char[][]{
-                { 0, 0, 0, 0 },
-                { 0, 0, 0, 0 },
-                { 0, 0, 0, 0 },
-                { 0, 0, 0, 0 }
+                { 'a', 'b', 'c', 'd'},
+                { 'e', 'F', 'G', 'H' },
+                { 'I', 'j', 'K', 'L' },
+                { 'M', 'n', 'O', 'P' }
         };
 
-        for (int iY = 0; iY < 4 ; iY++) {
-            for (int i = 0; i < 3; i++) {
-                for (int iX = 0; iX < 4; iX++) {
-                    System.out.print("  ");
-                    System.out.print("W");
-                    System.out.print("W");
-                    System.out.print("W");
-                    System.out.print("WW");
+        //Position an der sich der Spieler befindet
+        int xPosition = 0;
+        int yPosition = 0;
 
+        //Loop der fuer Jede Zeile einzeln Zeile 0 einmal ....
+        for (int iY = 0; iY < 4 ; iY++) {
+            //Der Wert wird dreimal ausgeben
+            for (int i = 0; i < 3; i++) {
+                //Jede Spalte in der Zeile
+                for (int iX = 0; iX < 4; iX++) {
+                        //Abfagen wenn der Spieler an der Positon ist die gerade geprinted wird dann ein 0
+                        if (xPosition == iX && yPosition == iY && i == 1) {
+                            System.out.print(kartenArray[iY][iX] + " 0 " + kartenArray[iY][iX]);
+                        } else{
+                            System.out.print(kartenArray[iY][iX] + " " + kartenArray[iY][iX] + " " + kartenArray[iY][iX]);
+                        }
                     System.out.print("  ");
                 }
-                System.out.println("");
+                System.out.println();
             }
-            System.out.println("");
+            System.out.println();
         }
 
     }
